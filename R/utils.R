37188dd6ab69b693dccf2092f7124fd1a4739f5d
jennybc <jenny@stat.ubc.ca>
2016-04-30 22:25:42 +0200

    use new cell_addr object from cellranger + whitespace
    I can probably make this entire function (A1_to_matrix) go away, once I understand things better

--- R/utils.R
+++ R/utils.R
@@ -13,34 +13,19 @@ vcapply <- function(X, FUN, ...) {
 
 ## Could contribute to cellranger?
 A1_to_matrix <- function(x) {
-  char0_to_NA <- cellranger:::char0_to_NA
-  rm_dollar_signs <- cellranger:::rm_dollar_signs
-
-  stopifnot(is.character(x))
-
-  x <- rm_dollar_signs(x)
-
-  m <- regexec("[[:digit:]]*$", x)
-  m <- regmatches(x, m)
-  row_part <- as.integer(vapply(m, char0_to_NA, character(1)))
-  row_part <- ifelse(row_part > 0, row_part, NA)
-
-  m <- regexec("^[[:alpha:]]*", x)
-  m <- regmatches(x, m)
-  col_part <- cellranger::letter_to_num(vapply(m, char0_to_NA, character(1)))
-
-  cbind(row=row_part, col=col_part)
+  ca <- cellranger::as.cell_addr_v(x, strict = FALSE)
+  cbind(row = cellranger::cell_row(ca), col = cellranger::cell_row(ca))
 }
 
 assert_list <- function(x, name=deparse(substitute(x))) {
   if (!is.list(x)) {
-    stop(sprintf("%s must be a list", name), call.=FALSE)
+    stop(sprintf("%s must be a list", name), call. = FALSE)
   }
 }
 
 assert_logical <- function(x, name=deparse(substitute(x))) {
   if (!is.logical(x)) {
-    stop(sprintf("%s must be logical", name), call.=FALSE)
+    stop(sprintf("%s must be logical", name), call. = FALSE)
   }
 }
 
@@ -49,14 +34,14 @@ assert_integer <- function(x, strict=FALSE, name=deparse(substitute(x))) {
     usable_as_integer <-
       !strict && is.numeric(x) && (max(abs(as.integer(x) - x)) < 1e-8)
     if (!usable_as_integer) {
-      stop(sprintf("%s must be integer", name), call.=FALSE)
+      stop(sprintf("%s must be integer", name), call. = FALSE)
     }
   }
 }
 
 assert_character <- function(x, name=deparse(substitute(x))) {
   if (!is.character(x)) {
-    stop(sprintf("%s must be character", name), call.=FALSE)
+    stop(sprintf("%s must be character", name), call. = FALSE)
   }
 }
 
@@ -69,17 +54,17 @@ assert_length <- function(x, n, name=deparse(substitute(x))) {
 assert_inherits <- function(x, what, name=deparse(substitute(x))) {
   if (!inherits(x, what)) {
     stop(sprintf("%s must be a %s", name,
-                 paste(what, collapse=" / ")), call.=FALSE)
+                 paste(what, collapse = " / ")), call. = FALSE)
   }
 }
 
 xr_to_idx <- function(xr) {
-  list(r=xr$ul[[1L]]:xr$lr[[1L]],
-       c=xr$ul[[2L]]:xr$lr[[2L]])
+  list(r = xr$ul[[1L]]:xr$lr[[1L]],
+       c = xr$ul[[2L]]:xr$lr[[2L]])
 }
 
 crayon_col <- function(fg, bg) {
   crayon::combine_styles(
     crayon::make_style(fg),
-    crayon::make_style(bg, bg=TRUE))
+    crayon::make_style(bg, bg = TRUE))
 }
